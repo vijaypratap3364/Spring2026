2ef18476c3fa3c514c1d6cb72b804bae8084de9b
vijaypratap3364 <singhvijaypratap1011@gmail.com>
2026-01-28 02:50:18 -0600

    lab 1 exe3 done

--- CS350/Labs/ex3.c
+++ CS350/Labs/ex3.c
@@ -1,3 +1,31 @@
+#***************************************
+#
+# Student Name: Vijay Pratap Singh
+#
+# Student Email:vpratapsingh@hawk.illinoistech.edu
+#
+# Course: CS 350 Computer Organization and Assembly Language Programming
+# Assignment: Lab-02
+#
+# Summary of Assignment Purpose: Introduction to C Programming Basics (datatypes, declaring variables∙input/output and loop operations)
+#
+# Date of Initial Creation: 01/23/2026
+#
+# Description of Program Purpose: able to learn declaring variables, basic 	data type operations, input/output operations,
+	writing a conditional statement, and basic loop operations.
+#
+# Functions and Modules in this file:
+		# Function/Module Name:
+		# Summary of Purpose:
+		# Input/Stored Value Requirements:
+		# Values Stored/Returned:
+
+# Additional Required Files:
+#
+#***************************************
+
+
+
 #include <stdio.h>
 
 int main()
